94d7596e54baafc4bfdb89a5b46cdca86ca5cb08
NAELKINDA <minseopark.dev@gmail.com>
2018-07-12 17:16:15 +0900

    #AlphaRelease 1.0.2

--- app/src/main/java/kr/hs/dimigo/meal/fragment/TodayMealViewFragment.java
+++ app/src/main/java/kr/hs/dimigo/meal/fragment/TodayMealViewFragment.java
@@ -3,11 +3,13 @@ package kr.hs.dimigo.meal.fragment;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import kr.hs.dimigo.meal.R;
 import kr.hs.dimigo.meal.communication.ConnectAPI;
@@ -48,6 +50,12 @@ public class TodayMealViewFragment extends Fragment{
     public void onActivityCreated(@Nullable Bundle savedInstanceState) {
         super.onActivityCreated(savedInstanceState);
 
+        if(savedInstanceState == null) {
+            Log.d("TEST", "savedInstanceState is null");
+        } else {
+            Log.d("TEST", "savedInstanceState is not null");
+        }
+
         //현재 날짜 표시
         todayDateTitle = getActivity().findViewById(R.id.todayDateTitle);
         todayDateTitle.setText(dateGenerator.dateTitleProvider(dateGenerator.getToday()));
@@ -73,56 +81,35 @@ public class TodayMealViewFragment extends Fragment{
                 // 아침시간
                 breakfastLamp.setImageResource(R.drawable.ic_lamp_on);
 
-                titleLunch.setTextColor(getResources().getColor(R.color.colorDisabled));
-                titleDinner.setTextColor(getResources().getColor(R.color.colorDisabled));
-                titleSnack.setTextColor(getResources().getColor(R.color.colorDisabled));
-
-                todayLunchMenuContent.setTextColor(getResources().getColor(R.color.colorDisabled));
-                todayDinnerMenuContent.setTextColor(getResources().getColor(R.color.colorDisabled));
-                todaySnackMenuContent.setTextColor(getResources().getColor(R.color.colorDisabled));
+                titleBreakfast.setTextColor(getResources().getColor(R.color.colorBlack));
+                todayBreakfastMenuContent.setTextColor(getResources().getColor(R.color.colorBlack));
                 break;
             case 1:
                 // 점심시간
                 lunchLamp.setImageResource(R.drawable.ic_lamp_on);
-
-                titleBreakfast.setTextColor(getResources().getColor(R.color.colorDisabled));
-                titleDinner.setTextColor(getResources().getColor(R.color.colorDisabled));
-                titleSnack.setTextColor(getResources().getColor(R.color.colorDisabled));
-
-                todayBreakfastMenuContent.setTextColor(getResources().getColor(R.color.colorDisabled));
-                todayDinnerMenuContent.setTextColor(getResources().getColor(R.color.colorDisabled));
-                todaySnackMenuContent.setTextColor(getResources().getColor(R.color.colorDisabled));
+                titleLunch.setTextColor(getResources().getColor(R.color.colorBlack));
+                todayLunchMenuContent.setTextColor(getResources().getColor(R.color.colorBlack));
                 break;
             case 2:
                 // 저녁시간
                 dinnerLamp.setImageResource(R.drawable.ic_lamp_on);
 
-                titleBreakfast.setTextColor(getResources().getColor(R.color.colorDisabled));
-                titleLunch.setTextColor(getResources().getColor(R.color.colorDisabled));
-                titleSnack.setTextColor(getResources().getColor(R.color.colorDisabled));
-
-                todayBreakfastMenuContent.setTextColor(getResources().getColor(R.color.colorDisabled));
-                todayLunchMenuContent.setTextColor(getResources().getColor(R.color.colorDisabled));
-                todaySnackMenuContent.setTextColor(getResources().getColor(R.color.colorDisabled));
+                titleDinner.setTextColor(getResources().getColor(R.color.colorBlack));
+                todayDinnerMenuContent.setTextColor(getResources().getColor(R.color.colorBlack));
                 break;
             case 3:
                 // 간식시간
                 snackLamp.setImageResource(R.drawable.ic_lamp_on);
 
-                titleBreakfast.setTextColor(getResources().getColor(R.color.colorDisabled));
-                titleLunch.setTextColor(getResources().getColor(R.color.colorDisabled));
-                titleDinner.setTextColor(getResources().getColor(R.color.colorDisabled));
-
-                todayBreakfastMenuContent.setTextColor(getResources().getColor(R.color.colorDisabled));
-                todayLunchMenuContent.setTextColor(getResources().getColor(R.color.colorDisabled));
-                todayDinnerMenuContent.setTextColor(getResources().getColor(R.color.colorDisabled));
+                titleSnack.setTextColor(getResources().getColor(R.color.colorBlack));
+                todaySnackMenuContent.setTextColor(getResources().getColor(R.color.colorBlack));
                 break;
             default:
                 break;
         }
 
         // 디미고인 API 사용을 통한 급식 정보 할당
-        // 추후 사용 데이터네트워크 절감을 위한 코드 최적화 예정
+        // 추후 데이터네트워크 사용량 절감을 위한 코드 최적화 예정
         ConnectAPI.apiService.getMealInfo(dateGenerator.getToday()).enqueue(new Callback<Pojo>() {
             @Override
             public void onResponse(Call<Pojo> call, Response<Pojo> response) {
@@ -137,7 +124,7 @@ public class TodayMealViewFragment extends Fragment{
 
             @Override
             public void onFailure(Call<Pojo> call, Throwable t) {
-
+                Toast.makeText(getContext(), "디미고인 API와의 통신이 불안정합니다.", Toast.LENGTH_LONG).show();
             }
         });
     }
